11464c1bbcf725f6f8a69ab74bf27f37235a1a03
conf-hao <2284635718@qq.com>
2025-07-22 12:05:09 +0800

    Class4: Object, class a brief introduction.

---
+++ EventSample/EventSample/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Windows.Threading;
+
+namespace EventSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            // This method will be called every second
+            // You can update the UI or perform other actions here
+            //MessageBox.Show("Timer ticked!");
+            this.timeTextBox.Text = DateTime.Now.ToString("hh:mm:ss tt");
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Console.WriteLine("Text changed in TextBox");
+        }
+    }
+}
